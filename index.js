9df02de213442804e1123348659bfc91f4ed9bd7
walter siqueira <robotdevelorper.py@gmail.com>
2021-01-08 19:55:55 +0000

    repetir uma vez!

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-//main statement
- let choice = prompt("escolha a operação a ser realizada: [1] [2] [3] [4] [exit]");
+main()
+function main() {
+//main statement block
+var choice = prompt("escolha a operação a ser realizada: [1] [2] [3] [4] [exit]");
 switch (choice) {
     
     case '1':
@@ -17,26 +19,39 @@ switch (choice) {
     case '4':
     div();
     break;
-}
 
+    case 'exit':
+    repeat();
+    break;
+}}
+    repeat();
 //list of functions
 function soma() {
     const n1 = Number(prompt("informe o primeiro valor: "))
     const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 + n2);
 }
+
 function subtração() {
     const n1 = Number(prompt("informe o primeiro valor: "))
     const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 - n2);
 }
+
 function multi() {
     const n1 = Number(prompt("informe o primeiro valor: "))
     const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 * n2);
 }
+
 function div() {
     const n1 = Number(prompt("informe o primeiro valor: "))
     const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 / n2)
 }
+
+function repeat() {
+var check = prompt("gostaria de continuar? [y]/[n]")
+if (check == "y") {
+    main()
+}}
