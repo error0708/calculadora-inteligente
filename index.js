89bbcedefeb54ed752ee6e7f5a9bb6bdfa382532
walter siqueira <robotdevelorper.py@gmail.com>
2021-01-08 19:38:18 +0000

    mais limpo e organizado

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-calculo();
-function decisão() {
-var confi =prompt ("gostaria de continuar calculando? [s/n]")
-if (confi == "s")  {
-    calculo()
-}}
-function calculo() {
-var resposta1 = prompt("escolha o tipo de operação que gostaria de realizar [+],[-],[x],[/]");
-
-if (resposta1 == "+") {
-    var n1 = parseInt(prompt("informe o primeiro valor a ser somado: "))
-    var n2 = parseInt(prompt("informe o segundo valor a ser somado: "))
-    alert(n1 + n2);
-    decisão()
+//main statement
+ let choice = prompt("escolha a operação a ser realizada: [1] [2] [3] [4] [exit]");
+switch (choice) {
     
+    case '1':
+    soma();
+    break;
+    
+    case '2':
+    subtração();
+    break;
+    
+    case '3':
+    multi();
+    break;
+   
+    case '4':
+    div();
+    break;
 }
-if (resposta1 == "-"){
-    var n1 = Number(prompt("informe o primeiro valor a ser subtraido: "))
-    var n2 = Number(prompt("informe o segundo valor a ser subtraido: "))
+
+//list of functions
+function soma() {
+    const n1 = Number(prompt("informe o primeiro valor: "))
+    const n2 = Number(prompt("informe o segundo valor: "))
+    alert (n1 + n2);
+}
+function subtração() {
+    const n1 = Number(prompt("informe o primeiro valor: "))
+    const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 - n2);
-    decisão()
 }
-if (resposta1 == "x") {
-    var n1 = Number(prompt("informe o primeiro valor a ser multiplicado: "))
-    var n2 = Number(prompt("informe o segundo valor a ser multiplicado: "))
+function multi() {
+    const n1 = Number(prompt("informe o primeiro valor: "))
+    const n2 = Number(prompt("informe o segundo valor: "))
     alert (n1 * n2);
-    decisão()
 }
-if (resposta1 == "/") {
-    var n1 = Number(prompt("informe o primeiro valor a ser dividido: "))
-    var n2 = Number(prompt("informe o segundo valor a ser dividido: "))
-    alert (n1 / n2);
-    decisão()
-}}
+function div() {
+    const n1 = Number(prompt("informe o primeiro valor: "))
+    const n2 = Number(prompt("informe o segundo valor: "))
+    alert (n1 / n2)
+}
